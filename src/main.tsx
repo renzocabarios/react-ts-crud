427a4f9e8534a64d3461490e5f8e4bc41e7c18af
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-10-25 18:47:02 +0800

    Add Provider in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { store } from './state/store'
+import { Provider } from 'react-redux'
+import { Home } from './page'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>
+  </Provider >
+
 )
